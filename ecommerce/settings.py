58dd91ae066d6329743b2fa35ebe818229783116
Prachi <sharma.01prachi02@gmial.com>
2025-08-31 14:02:33 +0530

    Set DEBUG to False for production

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ecommerce-tnpb.onrender.com', 'localhost', '127.0.0.1']
 
